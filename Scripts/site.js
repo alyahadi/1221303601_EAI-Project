13df1e7a0e010d8fb75e51614edda7001e7daf82
alyahadi <122496054+alyahadi@users.noreply.github.com>
2025-06-26 01:18:50 +0800

    Delete Scripts directory

--- Scripts/site.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    console.log("site.js is running!"); // <-- ADD THIS LINE
-
-    $('#resetFilterBtn').on('click', function () {
-        console.log("Reset button clicked!"); // <-- ADD THIS LINE to test the click
-        $('#categoryFilter').val('');
-        $(this).closest('form').submit();
-    });
-});
