459dd29f0739959298b15af86b94483eb7f89bb7
alyahadi <122496054+alyahadi@users.noreply.github.com>
2025-06-26 01:11:02 +0800

    Add files via upload

---
+++ Scripts/site.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    console.log("site.js is running!"); // <-- ADD THIS LINE
+
+    $('#resetFilterBtn').on('click', function () {
+        console.log("Reset button clicked!"); // <-- ADD THIS LINE to test the click
+        $('#categoryFilter').val('');
+        $(this).closest('form').submit();
+    });
+});
---
+++ ViewModels/CatalogViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AandN_Website.ViewModels
+{
+    public class CatalogViewModel
+    {
+        public string Category { get; set; }
+        public IntegrationServiceRef.CatalogItem[] Items { get; set; }
+    }
+}
